dc8a4a3ae79fbf3c525309362e948830b1d61074
crismon-01 <cristianmonti1995@gmail.com>
2020-12-13 12:36:24 +0100

    primo test accensione luce

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request
 import yaml
+import requests
+import json
 
 configinfo = yaml.load(open('./config.yaml'))
 serverport = configinfo['serverport']
+bridgeip = configinfo['bridgeip']
+authuser = configinfo['authuser']
+lampadinatest = configinfo['lampadinatest']
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','PUT'])
+@app.route('/', methods=['GET','POST'])
 def index():
-    # if request.methods == 'PUT':
-    #     request
+    if request.method == 'POST':
+        data = {}
+        data['on'] = True
+        req = requests.put("http://"+str(bridgeip)+"/api/"+authuser+"/lights/"+str(lampadinatest)+"/state", data=json.dumps(data))
+        print(str(req))
+        print(req.status_code)
     return render_template('index.html') #"Hello World!"
 
 if __name__ == '__main__':
