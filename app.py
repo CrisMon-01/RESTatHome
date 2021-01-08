f89ad41725bfb894e194da4d1c93df637bcdd0d4
crismon-01 <cristianmonti1995@gmail.com>
2021-01-08 18:01:39 +0100

    aggiunto stato vari groups e condizione gruppo acceso spento

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import yaml
 import requests
 import json
 
-configinfo = yaml.load(open('./config.yaml'))
+configinfo = yaml.load(open('./config.yaml'), Loader=yaml.FullLoader)
 serverport = configinfo['serverport']
 bridgeip = configinfo['bridgeip']
 authuser = configinfo['authuser']
@@ -16,10 +16,20 @@ def index():
     if request.method == 'POST':
         data = {}
         data['on'] = True
-        req = requests.put("http://"+str(bridgeip)+"/api/"+authuser+"/lights/"+str(lampadinatest)+"/state", data=json.dumps(data))
+        print("Nome Gruppo: "+str(request.form['name']))
+        req = requests.put("http://"+str(bridgeip)+"/api/"+authuser+"/groups/"+str(request.form.get('name'))+"/state", data=json.dumps(data))
         print(str(req))
         print(req.status_code)
-    return render_template('index.html') #"Hello World!"
+        return render_template('index.html')
+        # TO-DO GESTIRE PAGINA 
+    else:
+        req = requests.get("http://"+str(bridgeip)+"/api/"+authuser+"/groups")
+        lights = json.loads(req.content)
+        groups = []
+        for (k,v) in lights.items():
+            if not 'Group for' in str(v['name']):
+                groups.append((v['name'],v['state']['all_on']))
+        return render_template('index.html', groups=groups)
 
 if __name__ == '__main__':
     app.run(debug=True, port=serverport)
