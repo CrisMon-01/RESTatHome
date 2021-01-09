f1462b1f07a57a3666f4b4166b8978bdd01a4ff0
crismon-01 <cristianmonti1995@gmail.com>
2021-01-09 13:05:05 +0100

    aggiunto meccanismo gestione gruppi luci

--- app.py
+++ app.py
@@ -13,22 +13,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    req = requests.get("http://"+str(bridgeip)+"/api/"+authuser+"/groups")
+    lights = json.loads(req.content)
+    groups = []
+    for (k,v) in lights.items():
+        if not 'Group for' in str(v['name']):
+            groups.append((v['name'],v['state']['all_on']))
     if request.method == 'POST':
         data = {}
-        data['on'] = True
-        print("Nome Gruppo: "+str(request.form['name']))
-        req = requests.put("http://"+str(bridgeip)+"/api/"+authuser+"/groups/"+str(request.form.get('name'))+"/state", data=json.dumps(data))
-        print(str(req))
-        print(req.status_code)
-        return render_template('index.html')
-        # TO-DO GESTIRE PAGINA 
-    else:
-        req = requests.get("http://"+str(bridgeip)+"/api/"+authuser+"/groups")
-        lights = json.loads(req.content)
-        groups = []
+        print("Nome Gruppo: "+str(request.form.get('light')))
         for (k,v) in lights.items():
-            if not 'Group for' in str(v['name']):
-                groups.append((v['name'],v['state']['all_on']))
+            if str(request.form.get('light')) in str(v['name']):
+                status = v['state']['all_on']
+                nextstatus = not(bool(status))
+                data['on'] = nextstatus
+                req = requests.put("http://"+str(bridgeip)+"/api/"+authuser+"/groups/"+k+"/action", data=json.dumps(data))
+        return render_template('index.html', groups=groups)
+    else:
         return render_template('index.html', groups=groups)
 
 if __name__ == '__main__':
